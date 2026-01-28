ae95641ebf7cea2314596b0ecebe2de1d9c76bc9
XavierEgan <xavieregan2@gmail.com>
2026-01-28 20:56:05 +1100

    make image detection more robust

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ PLAY_WINDOW_SIZE_X = 1276
 PLAY_WINDOW_SIZE_Y = 716
 
 
-def get_window_pos(title="Clash of Clans"):
+def get_window_pos(title="Clash of Clans") -> tuple[int, int]:
     #get window handle
     window = gw.getWindowsWithTitle(title)[0]
     #set window to foreground
@@ -17,7 +17,7 @@ def get_window_pos(title="Clash of Clans"):
 
     if window:
         return window.left + OFFSET_X, window.top + OFFSET_Y
-    return None
+    raise Exception("No window found with title: " + title)
 
 def get_screenshot(start_x=0, start_y=0, end_x=PLAY_WINDOW_SIZE_X, end_y=PLAY_WINDOW_SIZE_Y):
     x, y = get_window_pos()
@@ -28,14 +28,16 @@ def get_screenshot(start_x=0, start_y=0, end_x=PLAY_WINDOW_SIZE_X, end_y=PLAY_WI
     return screenshot
 
 def init_clash_window():
-    if not get_window_pos():
+    try:
+        get_window_pos()
+    except Exception:
         pyautogui.press('win')
         sleep(0.2)
         pyautogui.write('Clash of Clans')
         sleep(0.2)
         pyautogui.press('enter')
-        sleep(15)  # Wait for the game to open
-
+        sleep(60)  # Wait for the game to open
+        print("opened Clash of Clans")
     windows = gw.getWindowsWithTitle("Clash of Clans")
 
     if not windows:
@@ -48,24 +50,32 @@ def init_clash_window():
         window.resizeTo(1280 + OFFSET_X, 720 + OFFSET_Y)
         sleep(0.1)
 
-def click_on_screen(x, y, max_offset=5, min_time=0.05, max_time=0.25, max_delay=0.5):
+def click_on_screen(x, y, max_offset=5, min_press_time=0.05, max_press_time=0.25, max_delay=0.5):
+    win_x, win_y = get_window_pos()
+    x += win_x
+    y += win_y
+
     offset_x = random.randint(-max_offset, max_offset)
     offset_y = random.randint(-max_offset, max_offset)
 
-    pyautogui.moveTo(x + offset_x, y + offset_y, duration=random.uniform(min_time, max_time))
+    pyautogui.moveTo(x + offset_x, y + offset_y)
 
     sleep(random.uniform(0, max_delay))
 
     pyautogui.mouseDown()
-    sleep(random.uniform(0.05, 0.2))
+    sleep(random.uniform(min_press_time, max_press_time))
     pyautogui.mouseUp()
 
 buttons = {
-    
+    "Attack!" : (71, 648, 10),
+    "Find a Match" : (144, 503),
+    "X Attack!" : (1083, 615)
 }
 
 if __name__ == "__main__":
-    #open_clash_window()
+    init_clash_window()
 
     screenshot = get_screenshot()
     screenshot.save("screenshot.png")
+
+    click_on_screen(*buttons["Attack!"])
