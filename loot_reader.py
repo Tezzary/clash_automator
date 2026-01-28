cc3ed4d4acdcdaf29f745fa845fd934c2a7cacd1
Zach <zacharyeverett.00@gmail.com>
2026-01-28 16:12:22 +1100

    basic structure

---
+++ loot_reader.py
@@ -0,0 +1,17 @@
+from utils import get_screenshot
+
+def get_elixir():
+    screenshot = get_screenshot()
+    pass
+def get_gold():
+    screenshot = get_screenshot()
+    pass
+def get_gems():
+    screenshot = get_screenshot()
+    pass
+
+if __name__ == "__main__":
+    elixir = get_elixir()
+    gold = get_gold()
+    gems = get_gems()
+    print(f"Elixir: {elixir}, Gold: {gold}, Gems: {gems}")
