e230cc3b7ce4e80f80e8cdf7194cec4e181f5718
XavierEgan <xavieregan2@gmail.com>
2026-01-28 17:33:19 +1100

    update random name generator

--- random_name.py
+++ random_name.py
@@ -54,20 +54,54 @@ clash_names = [
 def get_random_name():
     return random.choice(clash_names) + str(random.randint(1, 99999))
 
-if __name__ == "__main__":
+UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+SPECIAL = "!?@#$%&*"
+
+def generate_account_credentials():
     name = get_random_name()
     email = f"{name.lower()}@gmail.com"
-    password = str(abs(hash(name)) % (10 ** 12))
-    password_rand_indxes = [random.randint(0, len(password)-1) for _ in range(3)]
+    password = random.randint(10**15, 10**16 - 1)
+    password = str(password)
+
+    # get random indexes for password character replacement
+    password_rand_indxes = [i for i in range(16)]
+    random.shuffle(password_rand_indxes)
+    password_rand_indxes = password_rand_indxes[:8]
+
     password = list(password)
-    password[password_rand_indxes[0]] = chr(random.randint(65, 90))  # Uppercase
-    password[password_rand_indxes[1]] = chr(random.randint(97, 122)) # Lowercase
-    password[password_rand_indxes[2]] = chr(random.randint(33, 47))  # Special character
+
+    password[password_rand_indxes[0]] = random.choice(UPPERCASE)
+    password[password_rand_indxes[1]] = random.choice(LOWERCASE)
+    password[password_rand_indxes[2]] = random.choice(SPECIAL)
+    password[password_rand_indxes[3]] = random.choice(UPPERCASE)
+    password[password_rand_indxes[4]] = random.choice(LOWERCASE)
+    password[password_rand_indxes[5]] = random.choice(SPECIAL)
+    password[password_rand_indxes[6]] = random.choice(UPPERCASE)
+    password[password_rand_indxes[7]] = random.choice(SPECIAL)
+
     password = ''.join(password)
 
+    return name, email, password
 
-    print("Name:", name)
-    print("Email:", email)
-    print("Password:", password)
+if __name__ == "__main__":
+    temp = []
+    for _ in range(5):
+        name, email, password = generate_account_credentials()
+        temp.append((name, email, password))
     
-    print(f"{name} {email} {password}")
+    # print all names
+    for name, _, _ in temp:
+        print(f"{name}")
+    
+    print("---------------")
+
+    # print all emails
+    for _, email, _ in temp:
+        print(f"{email}")
+    
+    print("---------------")
+
+    # print all passwords
+    for _, _, password in temp:
+        print(f"{password}")
