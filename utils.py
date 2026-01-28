ee179706575af990a33a4e61362dd37aa85ef00e
Zach <zacharyeverett.00@gmail.com>
2026-01-28 16:22:20 +1100

    screenshots working, but not cropped to window yet

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ import pyautogui
 def get_screenshot():
     screenshot = pyautogui.screenshot()
     return screenshot
+
+if __name__ == "__main__":
+    screenshot = get_screenshot()
+    screenshot.show()
